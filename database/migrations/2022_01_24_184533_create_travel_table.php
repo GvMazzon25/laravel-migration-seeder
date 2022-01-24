24633094b186268012439b778035ed12fc1f5ae1
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-24 19:53:38 +0100

    create_table

---
+++ database/migrations/2022_01_24_184533_create_travel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('city');
+            $table->string('state');
+            $table->string('duration');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+}
