18b62563595a65841928f6e5fc6ad886ba217487
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-24 16:53:06 +0100

    connection-db

--- app/Travel.php
+++ app/Travel.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Travel extends Model
 {
-    //
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var string
+     */
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
 }
